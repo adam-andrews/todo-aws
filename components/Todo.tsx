05f36ba073a3cf2a2e0eb609006b9b90dbbe8c45
Adam Andrews <andrewsadam07@gmail.com>
2022-08-31 16:02:37 +0100

    Added todo

---
+++ components/Todo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Todo() {
+  return (
+<div className="alert alert-info shadow-lg max-w-3xl">
+  <div>
+  <input type="checkbox" className="checkbox bg-slate-100" />
+    <span>New software update available.</span>
+  </div>
+</div>
+  )
+}
+
+export default Todo
